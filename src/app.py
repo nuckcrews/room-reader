234d0570e5a7c4090c34c96db0d548763572fd0b
Nick Crews <nickcrews35@gmail.com>
2023-05-26 01:24:05 -0400

    Separate response

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ import os
 import logging
 import time
 from openai import ChatCompletion
+from .generate import generate_response
 from .extract import Extractor
 from .utils import *
 
@@ -29,39 +30,6 @@ def main():
             break
 
     announce("Goodbye", prefix="Bot: ")
-
-def generate_response(prompt: str, content: str):
-    try:
-        response = ChatCompletion.create(
-            model="gpt-4",
-            messages=[
-                {"role": "system", "content": "Respond based on the content from the file and the user prompt."},
-                {"role": "user", "content": f"Content:\n\n{content}"},
-                {"role": "user", "content": f"Prompt: {prompt}"},
-            ],
-            temperature=0.1,
-            stream=True
-        )
-
-        stripped = False
-        for event in response:
-            delta = event.get('choices')[0].get('delta')
-            event_text = None
-            if delta:
-                event_text = delta.get('content')
-
-            if not event_text:
-                continue
-
-            if not stripped:
-                event_text = event_text.strip()
-                stripped = True
-
-            stream(event_text)
-
-    except Exception as e:
-        logging.error(f"Error generating response: {e}")
-        return "An error occurred while generating the response. Please try again."
 
 def setup_logging():
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
---
+++ src/generate.py
@@ -0,0 +1,36 @@
+import logging
+from openai import ChatCompletion
+from utils import stream
+
+def generate_response(prompt: str, content: str):
+    try:
+        response = ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "Respond based on the content from the file and the user prompt."},
+                {"role": "user", "content": f"Content:\n\n{content}"},
+                {"role": "user", "content": f"Prompt: {prompt}"},
+            ],
+            temperature=0.1,
+            stream=True
+        )
+
+        stripped = False
+        for event in response:
+            delta = event.get('choices')[0].get('delta')
+            event_text = None
+            if delta:
+                event_text = delta.get('content')
+
+            if not event_text:
+                continue
+
+            if not stripped:
+                event_text = event_text.strip()
+                stripped = True
+
+            stream(event_text)
+
+    except Exception as e:
+        logging.error(f"Error generating response: {e}")
+        return "An error occurred while generating the response. Please try again."
